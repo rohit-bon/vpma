07c428cae9a4870a1e818508e1edf2ecec90c559
rohit-bon <rbongade01@gmail.com>
2023-07-23 00:25:07 +0530

    models completed

---
+++ lib/utils/components/ads_widget.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class AdsWidget extends StatelessWidget {
+  // final DatabaseManager _dbRef = DatabaseManager.getDbReference;
+  final bool isMobile;
+  AdsWidget({super.key, this.isMobile = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
